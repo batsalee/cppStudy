e93a0d82bdd9fceab13d930128f5a1c44745ecbf
batsalee <109213754+batsalee@users.noreply.github.com>
2022-11-08 21:45:44 +0900

    study

--- starcraft/starcraft/Marine.cpp
+++ starcraft/starcraft/Marine.cpp
@@ -13,7 +13,34 @@ Marine::Marine(int x, int y) {
     hp = 50;
     damage = 5;
     is_dead = false;
+
+    name = NULL;
 }
+Marine::Marine(int x, int y, const char* marine_name) {
+    name = new char[strlen(marine_name) + 1];
+    strcpy(name, marine_name);
+
+    coord_x = x;
+    coord_y = y;
+    hp = 50;
+    damage = 5;
+    is_dead = false;
+}
+Marine::Marine(int x, int y) {
+    coord_x = x;
+    coord_y = y;
+    hp = 50;
+    damage = 5;
+    is_dead = false;
+    name = NULL;
+}
+Marine::~Marine() {
+    std::cout << name << " 의 소멸자 호출 ! " << std::endl;
+    if (name != NULL) {
+        delete[] name;
+    }
+}
+
 void Marine::move(int x, int y) {
     coord_x = x;
     coord_y = y;
@@ -24,7 +51,7 @@ void Marine::be_attacked(int damage_earn) {
     if (hp <= 0) is_dead = true;
 }
 void Marine::show_status() {
-    std::cout << " *** Marine *** " << std::endl;
+    std::cout << " *** Marine : " << name << " ***" << std::endl;
     std::cout << " Location : ( " << coord_x << " , " << coord_y << " ) "
         << std::endl;
     std::cout << " HP : " << hp << std::endl;
--- starcraft/starcraft/Marine.h
+++ starcraft/starcraft/Marine.h
@@ -5,10 +5,13 @@ class Marine {
     int coord_x, coord_y;  // 마린 위치
     int damage;            // 공격력
     bool is_dead;
+    char* name;  // 마린 이름
 
 public:
     Marine();              // 기본 생성자
+    Marine(int x, int y, const char* marine_name);  // 이름까지 지정
     Marine(int x, int y);  // x, y 좌표에 마린 생성
+    ~Marine();
 
     int attack();                       // 데미지를 리턴한다.
     void be_attacked(int damage_earn);  // 입는 데미지
