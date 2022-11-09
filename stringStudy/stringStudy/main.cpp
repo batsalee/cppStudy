c4f6dc12ef480afd69b7226dac8fba832804fb09
batsalee <109213754+batsalee@users.noreply.github.com>
2022-11-09 22:01:01 +0900

    cpp 파일입출력

--- stringStudy/stringStudy/main.cpp
+++ stringStudy/stringStudy/main.cpp
@@ -1,53 +1,19 @@
+// 파일에서의 입출력
+#include <fstream>
 #include <iostream>
 #include <string>
 
-class Base 
-{
-private:
-	std::string s;
+int main() {
+    // 파일 읽기 준비
+    std::ifstream in("test.txt");
+    std::string s;
 
-public:
-	Base()
-	: s("기반")
-	{
-		std::cout << "기반 클래스" << std::endl;
-	}
-
-	void what()
-	{
-		std::cout << s << std::endl;
-	}
-};
-
-class Derived : public Base
-{
-private:
-	std::string s;
-
-public:
-	Derived()
-	: Base(), s("파생") 
-	{
-		std::cout << "파생 클래스" << std::endl;
-
-		// Base 에서 what() 을 물려 받았으므로
-		// Derived 에서 당연히 호출 가능하다
-		what();
-	}
-
-	void what()
-	{
-		std::cout << s << std::endl;
-	}
-};
-
-int main() 
-{
-	std::cout << " === 기반 클래스 생성 ===" << std::endl;
-	Base p;
-
-	std::cout << " === 파생 클래스 생성 ===" << std::endl;
-	Derived c;
-
-	return 0;
+    if (in.is_open()) {
+        in >> s;
+        std::cout << "입력 받은 문자열 :: " << s << std::endl;
+    }
+    else {
+        std::cout << "파일을 찾을 수 없습니다!" << std::endl;
+    }
+    return 0;
 }
