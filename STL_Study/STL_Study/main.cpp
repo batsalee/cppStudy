23a02cd1f227bcfa73769d1f4405b5b44acd67ab
batsalee <109213754+batsalee@users.noreply.github.com>
2022-11-10 23:51:15 +0900

    vector 사용해보기

--- STL_Study/STL_Study/main.cpp
+++ STL_Study/STL_Study/main.cpp
@@ -11,21 +11,37 @@ int main() {
     vec.push_back(30);
     vec.push_back(40);
 
-    for (auto itr = vec.begin(); itr != vec.end(); ++itr) {
+    for(auto itr = vec.begin(); itr != vec.end(); itr++) {
         std::cout << *itr << ' ';
     }
     std::cout << std::endl;
 
     vec.insert(vec.begin()+1, 15);
     // vec[1] = 15로 하고 2번부터 한칸씩 뒤로 밀기
-    for (auto itr = vec.begin(); itr != vec.end(); ++itr) {
+    // vector의 경우 insert나 erase하면 itr을 다시 설정해줘야 함
+    for(auto itr = vec.begin(); itr != vec.end(); itr++) {
         std::cout << *itr << ' ';
     }
     std::cout << std::endl;
 
     vec.erase(vec.begin()+1);
     // 다시 지우기
-    for (auto itr = vec.begin(); itr != vec.end(); ++itr) {
+    // vector의 경우 insert나 erase하면 itr을 다시 설정해줘야 함
+    for(auto itr = vec.begin(); itr != vec.end(); itr++) {
+        std::cout << *itr << ' ';
+    }
+    std::cout << std::endl;
+
+    // 특정 값을 찾아서 지우기
+    for (auto itr = vec.begin(); itr != vec.end(); itr++)
+    {
+        if (*itr == 30)
+        {
+            vec.erase(itr);
+            break;
+        }        
+    }
+    for (auto itr = vec.begin(); itr != vec.end(); itr++) {
         std::cout << *itr << ' ';
     }
     std::cout << std::endl;
