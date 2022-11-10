86b2a98389e513ce010fa6d9b778f7897de6e019
batsalee <109213754+batsalee@users.noreply.github.com>
2022-11-10 23:43:56 +0900

    vector 사용해보기

--- STL_Study/STL_Study/main.cpp
+++ STL_Study/STL_Study/main.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <list>
 
 int main() {
+    // vector 사용해보기
+    std::cout << "vector 사용해보기" << std::endl;
     std::vector<int> vec;
     vec.push_back(10);
     vec.push_back(20);
     vec.push_back(30);
     vec.push_back(40);
 
-    // auto itr = vec.begin() 하면 코드가 간결해진다
-    for (std::vector<int>::iterator itr = vec.begin(); itr != vec.end(); ++itr) {
-        std::cout << *itr << std::endl;
+    for (auto itr = vec.begin(); itr != vec.end(); ++itr) {
+        std::cout << *itr << ' ';
     }
+    std::cout << std::endl;
 
-    std::vector<int>::iterator itr = vec.begin() + 2;
-    std::cout << "3 번째 원소 :: " << *itr << std::endl;
+    vec.insert(vec.begin()+1, 15);
+    // vec[1] = 15로 하고 2번부터 한칸씩 뒤로 밀기
+    for (auto itr = vec.begin(); itr != vec.end(); ++itr) {
+        std::cout << *itr << ' ';
+    }
+    std::cout << std::endl;
+
+    vec.erase(vec.begin()+1);
+    // 다시 지우기
+    for (auto itr = vec.begin(); itr != vec.end(); ++itr) {
+        std::cout << *itr << ' ';
+    }
+    std::cout << std::endl;
 }
