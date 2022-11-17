6efedea186322084acdc5a932cb29945c187096a
batsalee <109213754+batsalee@users.noreply.github.com>
2022-11-17 09:13:18 +0900

    a

--- test/test/main.cpp
+++ test/test/main.cpp
@@ -1,65 +1,31 @@
-#include <string>
 #include <vector>
 #include <iostream>
-#include <algorithm>
-#include <map>
 
 using namespace std;
 
-vector<int> solution(vector<string> id_list, vector<string> report, int k) {
-
-    // 한명이 같은 사람 여러번 신고한 중복값 제거
-    report.erase(unique(report.begin(), report.end()), report.end());
-    
-    // report_map : <신고당한자, 신고한자>
-    multimap<string, string> report_map;
-    // count_reported : user가 신고당한 횟수 count
-    map<string, int> count_reported;
-
-    int index = 0;
-    string reporter = "";
-    string reported = "";
-    for (string r : report)
-    {
-        // 신고자, 신고당한자 분리
-        index = r.find(' ');
-        reporter = r.substr(0, index);
-        reported = r.substr(index + 1);
-
-        // <신고당한자, 신고한자> 형태로 insert
-        report_map.insert(pair<string, string>(reported, reporter));
-
-        // 신고당한자의 count_reported++
-        count_reported[reported]++;
-    }
-
-    vector<int> answer(id_list.size(), 0);
-
-    for (auto iter = count_reported.begin(); iter != count_reported.end(); iter++)
-    {
-        if (iter->second >= k) // k번이상 신고당했으면
-        {            
-            auto range = report_map.equal_range(iter->first);
-            for (auto itr = range.first; itr != range.second; itr++) {
-                // itr->second이 신고자의 아이디
-                auto it2 = find_if(id_list.begin(), id_list.end(), [itr](string s) {return (s==itr->second) ? true : false; });
-                // 신고자의 answer++;
-                answer[it2-id_list.begin()]++;
-            }
-        }
-    }
-    return answer;
-}
-
 void main()
 {
-    vector<string> list = { "muzi", "frodo", "apeach", "neo" };
-    vector<string> rep = { "muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi" };
-    
-    vector<int> output = solution(list, rep, 2);
+    int n = 3;
+    int array[3][3];
 
-    for (int i : output)
+    for (int i = 0; i < n; i++)
     {
-        cout << i << ' ';
+        for (int j = 0; j < n; j++)
+        {
+            array[i][j] = (i > j) ? i + 1 : j + 1;
+        }
     }
+
+    // 출력파트
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << array[i][j]<< ' ';
+        }
+        cout << endl;
+    }
+
+    int* array_ptr = &(array[0][0]);
+    cout << endl << *(array_ptr+5);
 }
