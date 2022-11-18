0245e9ea1447c80f159d7cb83d3cf5384915a332
batsalee <109213754+batsalee@users.noreply.github.com>
2022-11-18 23:45:46 +0900

    mutex

--- thread_study/thread_study/main.cpp
+++ thread_study/thread_study/main.cpp
@@ -1,32 +1,28 @@
 #include <iostream>
+#include <mutex>  // mutex 를 사용하기 위해 필요
 #include <thread>
-using std::thread;
+#include <vector>
 
-void func1() {
-    for (int i = 0; i < 10; i++) {
-        std::cout << "쓰레드 1 작동중! \n";
+void worker(int& result, std::mutex& m) {
+    for (int i = 0; i < 10000; i++) {
+        m.lock();
+        result += 1;
+        m.unlock();
     }
 }
 
-void func2() {
-    for (int i = 0; i < 10; i++) {
-        std::cout << "쓰레드 2 작동중! \n";
-    }
-}
-
-void func3() {
-    for (int i = 0; i < 10; i++) {
-        std::cout << "쓰레드 3 작동중! \n";
-    }
-}
 int main() {
-    thread t1(func1);
-    thread t2(func2);
-    thread t3(func3);
+    int counter = 0;
+    std::mutex m;  // 우리의 mutex 객체
 
-    t1.detach();
-    t2.detach();
-    t3.detach();
+    std::vector<std::thread> workers;
+    for (int i = 0; i < 4; i++) {
+        workers.push_back(std::thread(worker, std::ref(counter), std::ref(m)));
+    }
 
-    std::cout << "메인 함수 종료 \n";
+    for (int i = 0; i < 4; i++) {
+        workers[i].join();
+    }
+
+    std::cout << "Counter 최종 값 : " << counter << std::endl;
 }
