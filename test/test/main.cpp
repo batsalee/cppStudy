dd88a4375c9f59392d3976018535f125c60eded0
batsalee <109213754+batsalee@users.noreply.github.com>
2022-11-25 19:04:25 +0900

    constexpr 기능 확인 완료

--- test/test/main.cpp
+++ test/test/main.cpp
@@ -6,6 +6,7 @@ struct A {
 };
 
 int main() {
+	/*
 	constexpr int size = 3;
 	int arr[size];  // Good!
 
@@ -16,4 +17,16 @@ int main() {
 	constexpr int number = 3;
 	enum B { x = number, y, z };  // Good!
 	std::cout << B::x << std::endl;
+	*/
+
+	int n, m;
+	
+	// int array[n][m]; // 불가능
+	// constexpr int array[n][m]; // 불가능
+	n = 3, n = 2;
+	std::cin >> n >> m;
+	// int array[n][m]; // 불가능
+	constexpr int array[n][m]; // 가능
+	
+
 }
