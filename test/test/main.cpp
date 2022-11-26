9c3c8e6e664b2944d59d38b4e2decd2a1a6cc712
batsalee <109213754+batsalee@users.noreply.github.com>
2022-11-26 12:20:34 +0900

    chrono library

--- test/test/main.cpp
+++ test/test/main.cpp
@@ -19,14 +19,14 @@ int main() {
 	std::cout << B::x << std::endl;
 	*/
 
-	int n, m;
+	constexpr int n = 3, m = 3;
+
+	// std::cin >> n >> m; // constexpr도 const라 불가능하고
+	int array[n][m]; // 이런식으로 배열선언이 아니라 변수쪽에 constexpr 붙여줘야 함
 	
-	// int array[n][m]; // 불가능
-	// constexpr int array[n][m]; // 불가능
-	n = 3, n = 2;
-	std::cin >> n >> m;
-	// int array[n][m]; // 불가능
-	constexpr int array[n][m]; // 가능
+	// 결국 재사용할때 오류 방지를 위해 변수로 쓸 경우 constexpr 쓰면 변수 하나로만 수정 가능하다는거고
+	// 하나만 그냥 쓸땐 그냥 숫자 넣으면 되는 것
+	// 그냥 이런 기능이 있다만 알아 두면 될 듯
 	
 
 }
