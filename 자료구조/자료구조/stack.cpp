04a333687db6eec787502db24d1920f227a6e01b
batsalee <109213754+batsalee@users.noreply.github.com>
2022-12-18 18:24:08 +0900

    array

--- 자료구조/자료구조/stack.cpp
+++ 자료구조/자료구조/stack.cpp
@@ -1,32 +1,18 @@
 #include <iostream>
-#include <stack>
-#include <queue>
+#include <array>
 
 int main()
 {
-	/*
-	std::stack<int> s;
+	std::array<int, 3> a = { 1, 2, 3 };
+	std::array<int, 3> b;
 
-	s.push(3);
-	s.push(2);
-	s.push(1);
+	b = a;
 
-	std::cout << s.top(); // 1 출력
+	std::cout << a.front() << ' ' << a.back() << std::endl;
 
-	s.pop(); // 1 삭제
-	s.pop(); // 2 삭제
-
-	s.size(); // 스택 사이즈 반환
-
-	std::cout << std::boolalpha;
-	std::cout << s.empty(); // 비어있으면 true, 아니면 false 반환
-	*/
-
-	std::queue<int> q;
-	q.push(1);
-	q.push(2);
-	q.push(3);
-
-	std::cout << q.front() << '\n'; // 1 출력
-	std::cout << q.back() << '\n'; // 3 출력
+	std::array<int, 5> c;
+	c.fill(1);
+	for (int i : c) {
+		std::cout << i << ' ';
+	}
 }
